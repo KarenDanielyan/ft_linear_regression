40864b78260f86f070b33e8851a9315a3e54971c
Karen Danielyan <kdaniely@b-phot.org>
2026-06-12 00:55:15 +0200

    feat: model training without data preprocessing

---
+++ train.py
@@ -0,0 +1,10 @@
+# Training the linear regression model
+# imports
+import pandas as pd
+import numpy as np
+
+
+# main
+if __name__ == '__main__':
+    # Load the data
+    data = pd.read_csv("data.csv");
